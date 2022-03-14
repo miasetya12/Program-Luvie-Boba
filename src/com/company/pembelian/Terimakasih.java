bf5416a0c80e8333c7700f9c66201b6d26c89725
miasetya12 <miasetya12@gmail.com>
2022-03-14 20:59:11 +0800

    First Commit

--- src/com/company/pembelian/Terimakasih.java
+++ src/com/company/pembelian/Terimakasih.java
@@ -1,17 +1,14 @@
 package com.company.pembelian;
 import com.company.Main;
-
 import java.util.*;
 
 public class Terimakasih {
     public static void ucapan() {
         char pilihan;
         Scanner aa = new Scanner(System.in);
+        System.out.println("\n================================");
         System.out.println("Mohon ditunggu, pesanan segera datang!");
-        System.out.println("Mohon ditunggu, pesanan segera datang!");
-
-        System.out.println("================================");
-        System.out.print("Ingin memesan kembali? (y/n) :  ");
+        System.out.println("Ingin memesan kembali? (y/n) :  ");
         pilihan = (char) aa.nextInt();
         if (pilihan == 'Y' || pilihan == 'y') {
             Main.main();
